47501d2c13103e04245a188e51830c7bd7efabf4
Alejadro Sanchez-Giraldo <morsisdivine@gmail.com>
2025-07-28 22:18:46 +1000

    enable parallel

--- ts-src/playwright.config.ts
+++ ts-src/playwright.config.ts
@@ -5,10 +5,11 @@ export default defineConfig({
   timeout: 30000,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : 4,
+  workers: process.env.CI ? 2 : 4,
   fullyParallel: true, // Run all tests in parallel
   reporter: 'html',
   use: {
+    // headless: false,
     actionTimeout: 0,
     trace: 'on-first-retry',
     screenshot: 'on',
